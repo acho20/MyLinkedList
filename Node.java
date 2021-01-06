9ffd42b7f978141672b7ea542e0977ff38c69ca5
Alex Cho <acho20@stuy.edu>
2021-01-06 15:42:17 -0500

    created functions for Node.java

--- Node.java
+++ Node.java
@@ -5,6 +5,27 @@ public class Node{
     prev = null;
   }
 
+  public String getData(){
+    return data;
+  }
+  public void setData(String value){
+    data = value;
+  }
+
+  public Node getNext(){
+    return next;
+  }
+  public Node getPrev(){
+    return prev;
+  }
+
+  public void setNext(String value){
+    next = new Node(value);
+  }
+
+  public void setPrev(String value){
+    prev = new Node(value);
+  }
   private String data;
   private Node next,prev;
 }
