660fae4e36cbd3792cb400d4bfb6ca95da4b6257
Alex Cho <acho20@stuy.edu>
2021-01-10 15:59:41 -0500

    fixed major issues

--- MyLinkedList.java
+++ MyLinkedList.java
@@ -13,60 +13,101 @@ public class MyLinkedList{
     size++;
     if(start == null) {
       start = new Node(value);
+      start.setNext(end);
     }
     else if(end == null) {
       end = new Node(value);
-      start.setNext(end.getData());
-      end.setPrev(start.getData());
+      start.setNext(end);
+      end.setPrev(start);
     }
     else
     {
-      Node temp = end;
-      end.getPrev().setNext(temp.getData());
-      temp.setNext(value);
-      end = temp.getNext();
+      Node temp = new Node(value);
+      temp.setPrev(end);
+      end.setNext(temp);
+      end = temp;
     }
     return true;
   }
-  public boolean add(int index, String value){
+  public void add(int index, String value){
     Node temp = start;
     Node temp2;
     if(index > size){
-      return false;
-    }
-    else{
-      for(int i = 0; i < index; i++){
+      return;
+    }else if (index == size){
+      add(value);
+    }else{
+      size++;
+      for(int i = 0; i < index - 1; i++){
         temp = temp.getNext();
       }
+      // Might run into IndexOutOfBounds below
+      // consider using end
       temp2 = temp.getNext();
-      temp.setNext(value);
-      temp.getNext().setNext(temp2.getData());
-      temp.getNext().setPrev(temp.getData());
-      temp2.setPrev(temp.getNext().getData());
-      return true;
+      temp.setNext(new Node(value));
+      temp.getNext().setNext(temp2);
+      temp.getNext().setPrev(temp);
+      temp2.setPrev(temp.getNext());
+      return;
     }
   }
   public String get(int index){
-    Node temp = start;
-    for(int i = 0; i < index; i++){
-      temp = temp.getNext();
+    try{
+      if(index >= size){
+        throw new Exception("IndexOutOfBounds");
+      }
+      Node temp = start;
+      for(int i = 0; i < index; i++){
+        temp = temp.getNext();
+      }
+      return temp.getData();
     }
-    return temp.getData();
+    catch (Exception e) {
+      e.printStackTrace();
+    }
+    return null;
+  }
+  private Node getNode(int index){
+    try{
+      if(index >= size){
+        throw new Exception("IndexOutOfBounds");
+      }
+      Node temp = start;
+      for(int i = 0; i < index; i++){
+        temp = temp.getNext();
+      }
+      return temp;
+    }
+    catch(Exception e) {
+      e.printStackTrace();
+      return null;
+    }
   }
   public String set(int index, String value){
-    Node temp = new Node (get(index));
-    String x = temp.getData();
-    temp.setData(value);
-    return x;
+    try{
+      if(index >= size) {
+        throw new Exception("IndexOutOfBounds");
+      }
+      this.getNode(index).setData(value);
+      return value;
+    }
+    catch(Exception e) {
+      e.printStackTrace();
+      return null;
+    }
   }
   public String toString(){
-    String x = "";
+    if(start == null) {
+      return "";
+    }
+    String x = start.getData();
     Node temp = start;
     for(int i = 0; i < size - 1; i++){
-      x += temp.getData();
       x += ", ";
+      temp = temp.getNext();
+      x += temp.getData();
     }
-    x += end.getData();
     return x;
   }
+  //Any helper method that returns a Node object MUST BE PRIVATE!
 }
--- Node.java
+++ Node.java
@@ -1,17 +1,19 @@
 public class Node{
+  // stored variables for Node
+  private String data;
+  private Node next,prev;
+
+  // basic constructor
   public Node(String value){
     data = value;
     next = null;
     prev = null;
   }
 
+  // accessor (getter) functions
   public String getData(){
     return data;
   }
-  public void setData(String value){
-    data = value;
-  }
-
   public Node getNext(){
     return next;
   }
@@ -19,13 +21,18 @@ public class Node{
     return prev;
   }
 
-  public void setNext(String value){
-    next = new Node(value);
+  // mutator (setter) functions
+  public void setData(String value){
+    data = value;
+  }
+  public void setNext(Node n){
+    next = n;
+  }
+  public void setPrev(Node n){
+    prev = n;
   }
 
-  public void setPrev(String value){
-    prev = new Node(value);
-  }
-  private String data;
-  private Node next,prev;
+
+
+
 }
